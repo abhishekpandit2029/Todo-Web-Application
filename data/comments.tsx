d912d8f8e7bcf7ebd441fb3d7ed49a66a56e40ee
abhi <abhishekpandit1212@gmail.com>
2023-06-29 15:08:04 +0530

    set css to todo

--- data/comments.tsx
+++ data/comments.tsx
@@ -1,11 +1,3 @@
-// export const comments = [
-//     {
-//       id: 1,
-//       text: "Today, I'll relish my day."
-//     },
-//   ] 
-
-
 export const comments: { id: number, text: string }[] = [
   {
       id: 1,
